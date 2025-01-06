795716e5a37faffc9ca49a6526ae812dfbb17076
Killian <martykillian7@gmail.com>
2025-01-06 13:51:19 +0100

    include billboards

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -19,6 +19,9 @@ export async function GET(
             where: {
                 id: params.categoryId
             },
+            include: {
+                billboard: true,
+            }
         });
 
         // response
