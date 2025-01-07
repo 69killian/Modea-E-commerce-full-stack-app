e5987799583f725dd13935099a80d1d6bff06bdf
Killian <martykillian7@gmail.com>
2025-01-07 21:14:02 +0100

    dependancies typescript

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,15 +1,17 @@
 import prismadb from "@/prisma/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
-// Définir explicitement les types des props
-const BillboardsPage = async ({
-    params,
-}: {
-    params: { storeId: string; billboardId: string };
-}) => {
+// Typage correct des props
+interface PageProps {
+    params: {
+        storeId: string;
+        billboardId: string;
+    };
+}
+
+const BillboardsPage = async ({ params }: PageProps) => {
     const { billboardId } = params;
 
-    // Récupérer les données de la base de données
     const billboard = await prismadb.billboard.findUnique({
         where: { id: billboardId },
     });
