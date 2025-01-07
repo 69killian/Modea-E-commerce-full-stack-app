0835d63b06d11dc4a7652274d5fd79e6947b6677
Killian <martykillian7@gmail.com>
2025-01-07 21:09:51 +0100

    params next15

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,6 +1,7 @@
 import prismadb from "@/prisma/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
+// Définir explicitement les types des props
 const BillboardsPage = async ({
     params,
 }: {
@@ -8,6 +9,7 @@ const BillboardsPage = async ({
 }) => {
     const { billboardId } = params;
 
+    // Récupérer les données de la base de données
     const billboard = await prismadb.billboard.findUnique({
         where: { id: billboardId },
     });
