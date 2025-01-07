1285778da7c898f0949aa4ec6392c2d5ede1513b
Killian <martykillian7@gmail.com>
2025-01-07 21:17:10 +0100

    getserverside props to trhe billboard page

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,21 +1,12 @@
 import prismadb from "@/prisma/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
-// Typage correct des props
 interface PageProps {
-    params: {
-        storeId: string;
-        billboardId: string;
-    };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    billboard: any;
 }
 
-const BillboardsPage = async ({ params }: PageProps) => {
-    const { billboardId } = params;
-
-    const billboard = await prismadb.billboard.findUnique({
-        where: { id: billboardId },
-    });
-
+const BillboardsPage = ({ billboard }: PageProps) => {
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
@@ -24,5 +15,21 @@ const BillboardsPage = async ({ params }: PageProps) => {
         </div>
     );
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function getServerSideProps(context: any) {
+    const { billboardId } = context.params;
+
+    // Récupérer les données avec prisma
+    const billboard = await prismadb.billboard.findUnique({
+        where: { id: billboardId },
+    });
+
+    return {
+        props: {
+            billboard,
+        },
+    };
+}
 
 export default BillboardsPage;
