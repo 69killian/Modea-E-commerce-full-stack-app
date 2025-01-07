7f775cf802d96600b411c6b79ca4e2bcb3010349
Killian <martykillian7@gmail.com>
2025-01-07 14:40:18 +0100

    Stripe payment checkout

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,4 +1,14 @@
 import prismadb from "@/prisma/prismadb";
+import { Heading } from "@/components/ui/Heading";
+import { Separator } from "@radix-ui/react-separator";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { CreditCard, DollarSign, Package } from "lucide-react";
+import { formatter } from "@/lib/utils";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getStockCount } from "@/actions/get-stock-count copy";
+import { Overview } from "@/components/overview";
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
 
 interface DashboardPageProps {
     params: { storeId: string}
@@ -12,9 +22,67 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({
             id: params.storeId
         }
     })
+
+    const totalRevenue = await getTotalRevenue(params.storeId);
+    const salesCount = await getSalesCount(params.storeId);
+    const stockCount = await getStockCount(params.storeId);
+    const graphRevenue = await getGraphRevenue(params.storeId);
+
     return (
-        <div>
-            Active Store: {store?.name}
+        <div className="flex-col p-6">
+            <div className="flex-1 space-y-4 py-8 pt-6">
+                <Heading title="Dashboard" description="Vue d'ensemble de votre Boutique"/>
+                <Separator/>
+                <div className="grid gap-4 grid-cols-3 ">
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                            Revenu Total
+                            </CardTitle>
+                            <DollarSign className="h-4 w-4 text-muted-foreground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold ">
+                                {formatter.format(totalRevenue)}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                            Ventes
+                            </CardTitle>
+                            <CreditCard className="h-4 w-4 text-muted-foreground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold ">
+                                +{salesCount}
+                            </div>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                            <CardTitle className="text-sm font-medium">
+                            Produits en Stock
+                            </CardTitle>
+                            <Package className="h-4 w-4 text-muted-foreground"/>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="text-2xl font-bold ">
+                            {stockCount}
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+                <Card className="col-span-4">
+                    <CardHeader>
+                        <CardTitle>Overview</CardTitle>
+                    </CardHeader>
+                    <CardContent className="pl-2">
+                        <Overview data={graphRevenue}/>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
     );
 }
