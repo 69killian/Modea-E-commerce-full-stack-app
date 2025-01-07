4b6933a4d77b38c75d09e297adcdeb9573f94b3e
KillianCodes <145566954+69killian@users.noreply.github.com>
2025-01-07 22:34:58 +0100

    Removed pageProps

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,35 +1,19 @@
-import { format } from 'date-fns';
+import prismadb from "@/prisma/prismadb";
+import { BillboardForm } from "./components/billboard-form";
 
-import prismadb from '@/prisma/prismadb';
-import { BillboardClient } from './components/client';
-import { BillboardColumn } from './components/columns';
+// Utiliser directement les paramètres dans le composant
+const BillboardsPage = async ({ params }: { params: { storeId: string; billboardId: string } }) => {
+    const { billboardId } = params;
 
-const BillboardsPage = async ({
-    params
-}: {
-    params: { storeId: string }
-}) => {
-    
-    const billboards = await prismadb.billboard.findMany({
-        where: {
-            storeId: params.storeId,
-        },
-        orderBy: {
-            createdAt: "desc",
-        },
+    // Récupérer les données avec Prisma
+    const billboard = await prismadb.billboard.findUnique({
+        where: { id: billboardId },
     });
-
-    
-    const formattedBillboards: BillboardColumn[] = billboards.map((item) => ({
-        id: item.id,
-        label: item.label,
-        createdAt: format(item.createdAt, "MMMM do, yyyy"),
-    }));
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardClient data={formattedBillboards} />
+                <BillboardForm initialData={billboard} />
             </div>
         </div>
     );
