1b92ab55b12ed8e31cbf8ea486949b014d4e6600
Killian <martykillian7@gmail.com>
2025-01-07 21:02:23 +0100

    fix params props

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,25 +1,21 @@
+import { GetServerSidePropsContext } from "next";
 import prismadb from "@/prisma/prismadb";
-
 import { BillboardForm } from "./components/billboard-form";
 
-const BillboardsPage = async ({
-    params
-}: {
-    params: { billboardId: string }
-}) => {
+const BillboardsPage = async ({ params }: GetServerSidePropsContext) => {
+    const billboardId = params?.billboardId as string;
+
     const billboard = await prismadb.billboard.findUnique({
-        where: {
-            id: params.billboardId
-        }
+        where: { id: billboardId },
     });
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                <BillboardForm initialData={billboard}/>
+                <BillboardForm initialData={billboard} />
             </div>
         </div>
     );
-}
+};
 
 export default BillboardsPage;
