0dc93f626bde2fbaed16a5b60195cc030dce405c
Killian <martykillian7@gmail.com>
2025-01-08 11:10:59 +0100

    delete props of routes page.tsx

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -14,8 +14,8 @@ interface DashboardPageProps {
     params: { storeId: string}
 };
 
-const DashboardPage: React.FC<DashboardPageProps> = async props => {
-    const params = await props.params;
+const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+
 
     const totalRevenue = await getTotalRevenue(params.storeId);
     const salesCount = await getSalesCount(params.storeId);
